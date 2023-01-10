6b125737b6943d60468aafe1b953d298e1cb96b3
Andrej2003 <andrej.nedelkovski.an@gmail.com>
2023-01-10 21:25:05 +0100

    create route read_data & add imports

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
+import sqlite3
+import pymongo
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/data', methods=["GET"])
+def read_data():
+    db = sqlite3.connect("data.db")
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM companies")
+    data = cursor.fetchall()
+    return data
+
+
+# @app.route('/write_data', methods=["POST"])
+# def write_data():
+#     client = pymongo.MongoClient("mongodb://localhost:27017")
+#     db = client['purified_data']
 
 
 if __name__ == '__main__':
