45f0300fa78aeefdcadd2e431780135218a79645
Andrej2003 <andrej.nedelkovski.an@gmail.com>
2023-01-12 20:52:30 +0100

    test calls to the api

--- calls_api.py
+++ calls_api.py
@@ -1,5 +1,7 @@
 import requests
 
-company = requests.get("http://localhost:5000/1").json()
+company = requests.get("http://localhost:5000/companies-data").json()
 print(type(company))
 print(company)
+
+test_company_id = requests.post("http://localhost:5000/companies-data/purify", data={"id": 1234})
