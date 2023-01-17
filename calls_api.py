cd7ccb3c13011c0b6830030d3e2552afbe5c37c9
Andrej2003 <andrej.nedelkovski.an@gmail.com>
2023-01-17 21:25:33 +0100

    refactor and fix bugs in clean_names function

--- calls_api.py
+++ calls_api.py
@@ -9,6 +9,7 @@ companies_data = requests.get("http://localhost:5000/companies-data").json()
 
 
 def clean_names():
+    r = re.compile("[(].*?[)]")
     s = re.compile("[.]$")
     t = re.compile("plc", flags=re.IGNORECASE)
     u = re.compile("company", flags=re.IGNORECASE)
@@ -20,72 +21,38 @@ def clean_names():
     names = [i[1].lower() for i in companies_data]
 
     for i, j in enumerate(names):
+        if bool(r.search(j)):
+            j = r.sub("", j)
+
         if bool(s.search(j)):
             j = s.sub("", j)
 
         if bool(t.search(j)):
-            j = j.replace(t.search(j).group(), "").strip("()").title().strip()
+            j = j.replace(t.search(j).group(), "")
 
         # if bool(u.search(j)):
         #     j = j.replace(u.search(j).group(), "").strip("()").title().strip()
 
         if bool(v.search(j)):
-            j = j.replace(v.search(j).group(), "").strip().strip("()").title().strip()
+            j = j.replace(v.search(j).group(), "")
 
         if bool(w.search(j)):
-            j = j.replace(w.search(j).group(), "").strip().strip("()").title().strip()
+            j = j.replace(w.search(j).group(), "")
 
         if bool(x.search(j)):
-            j = j.replace(x.search(j).group(), "").strip().strip("()").title().strip()
+            j = j.replace(x.search(j).group(), "")
 
         if bool(y.search(j)):
-            j = j.replace(y.search(j).group(), "").strip().strip("()").title().strip()
+            j = j.replace(y.search(j).group(), "")
 
         if bool(z.search(j)):
-            j = j.replace(z.search(j).group(), "").strip().strip("()").title().strip()
+            j = j.replace(z.search(j).group(), "")
+
+        j = " ".join(j.split()).title()
 
         names[i] = j
 
     return names
-
-# def clean_names():
-#     u = re.compile("company", flags=re.IGNORECASE)
-#     v = re.compile("llp", flags=re.IGNORECASE)
-#     w = re.compile("uk", flags=re.IGNORECASE)
-#     x = re.compile("limited", flags=re.IGNORECASE)
-#     y = re.compile("ltd", flags=re.IGNORECASE)
-#     z = re.compile("ltd.", flags=re.IGNORECASE)
-#     names = [i[1].lower() for i in companies_data]
-#     name = names[0]
-#
-#     for i, j in enumerate(names[0:1]):
-#         if bool(u.search(j)):
-#             names[i] = j.replace(u.search(j).group(), "").strip().strip("()").title()
-#             print("inside u", names[0])
-#
-#         if bool(v.search(j)):
-#             names[i] = j.replace(v.search(j).group(), "").strip().strip("()").title()
-#             print("inside v", names[0])
-#
-#         if bool(w.search(j)):
-#             names[i] = j.replace(w.search(j).group(), "").strip().strip("()").title()
-#             print("inside w", names[0])
-#         print("after w", names[0])
-#         if bool(x.search(j)):
-#             names[i] = j.replace(x.search(j).group(), "").strip().strip("()").title()
-#             print("inside x", names[0])
-#         print("before y", names[0])
-#         if bool(y.search(j)):
-#             print("index", names[i])
-#             names[i] = j.replace(y.search(j).group(), "").strip().strip("()").title()
-#             print("inside y", names[0])
-#         print('after y', names[0])
-#
-#         if bool(z.search(j)):
-#             names[i] = j.replace(z.search(j).group(), "").strip().strip("()").title()
-#             print("inside z", names[0])
-#
-#     return names
 
 
 def clean_names2():
