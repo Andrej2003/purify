75d1bd622ab19b459352fee127b7c10ac2d7eaa8
Andrej2003 <andrej.nedelkovski.an@gmail.com>
2023-02-10 21:48:44 +0100

    write docstrings

--- app.py
+++ app.py
@@ -9,6 +9,11 @@ app = Flask(__name__, static_url_path="/static")
 @app.route('/')
 @app.route('/companies-data', methods=["GET"])
 def read_data_template():
+    """ Renders a template (or view) for the companies-data endpoint.
+
+    :return: str
+    """
+
     db = sqlite3.connect("data.db")
     cursor = db.cursor()
     cursor.execute(f"SELECT * FROM companies")
@@ -20,6 +25,14 @@ def read_data_template():
 @app.route('/api')
 @app.route('/api/companies-data', methods=["GET"])
 def read_data():
+    """
+    GET method
+
+    Returns a list containing the data and column names on the api/companies-data endpoint.
+
+    :return:  list
+    """
+
     db = sqlite3.connect("data.db")
     cursor = db.cursor()
     cursor.execute(f"SELECT * FROM companies")
@@ -30,6 +43,12 @@ def read_data():
 
 @app.route('/companies-data/purify', methods=["GET"])
 def write_data_view():
+    """
+    Renders a template (or view) for the companies-data/purify endpoint.
+
+    :return: str
+    """
+
     client = pymongo.MongoClient("mongodb://localhost:27017")
     db = client['purified_data']['companies']
     data = [x for x in db.find({}, {"_id": 0})]
@@ -39,6 +58,14 @@ def write_data_view():
 
 @app.route('/api/companies-data/purify', methods=["POST"])
 def write_data():
+    """
+    POST method
+
+    Writes the purified data in the MongoDB database.
+
+    :return: str
+    """
+
     clean_data = json.loads(request.json)
 
     client = pymongo.MongoClient("mongodb://localhost:27017")
